9238fea349bf59d470689c86617065839764d92e
alexe <prograbota@mail.ru>
2022-11-13 21:20:17 +0300

    new person

--- src/main/java/ru/spiridonov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/spiridonov/springcourse/controllers/PeopleController.java
@@ -4,10 +4,9 @@ import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.spiridonov.springcourse.dao.PersonDAO;
+import ru.spiridonov.springcourse.moduls.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -34,5 +33,16 @@ public class PeopleController {
         return "people/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        //model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 
 }
